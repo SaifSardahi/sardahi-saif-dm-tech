3933fe1a1a48bb621b650c507f86a98ba69b4f95
SaifSardahi <saifsardahi@gmail.com>
2022-09-28 14:42:55 +0200

    finish appenchild

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 let showElement = document.getElementById('main');
+
+const addElement1 = document.createElement('h5')
+const addElement2 = document.createElement('h5')
+
+showElement.appendChild(addElement1);
+showElement.appendChild(addElement2);
+
 showElement.innerHTML;
 console.log(showElement);
